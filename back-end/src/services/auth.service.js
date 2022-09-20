b467f0d4392df4be534f1177ef8b16f292a99e50
Caique Moreira <caique_moreira_sz@hotmail.com>
2022-09-20 20:33:18 -0300

    fix: auth.service model name

--- back-end/src/services/auth.service.js
+++ back-end/src/services/auth.service.js
@@ -3,11 +3,11 @@ const JwtUtilities = require('../utils/JwtUtilities')
 
 // import JwtUtilities from '../utils/JwtUtilities'
 
-const { User }  = require('../database/models');
+const { users }  = require('../database/models');
 
 const validateCredentials = async ( email, password ) => {
 
-  const user = await User.findOne({ where: { email, password } });
+  const user = await users.findOne({ where: { email, password } });
   if (!user) {
     const e = new Error('Invalid fields');
     e.name = 'ValidationError';
