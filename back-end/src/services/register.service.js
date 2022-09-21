6a1fafc22ffe6a783d6e84dfe6ad11481f186005
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2022-09-21 18:19:46 -0300

    fix: :bug: Refactored register service

--- back-end/src/services/register.service.js
+++ back-end/src/services/register.service.js
@@ -2,14 +2,13 @@ require('dotenv/config');
 const { users } = require('../database/models');
 
 const registerNewUser = async (username, email, password) => {
-  await users.create({ username, email, password, role: 'customer' });
-
   const newUser = await users.findOne({ where: { email, username } });
   if (newUser) {
     const e = new Error('User already exists');
     e.name = 'Conflict';
     throw e;
   }
+  await users.create({ username, email, password, role: 'customer' });
 };
 
 module.exports = {
