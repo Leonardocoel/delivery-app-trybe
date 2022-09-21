0ee64155e6d73eeca5b924a246176f314352a289
Leonardo C <leonardocoel98@gmail.com>
2022-09-21 16:16:41 -0300

    feat: implement swaggerUi and add /docs endpoint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
+
 const authRouter = require('../router/auth.router');
 const errorHandler = require('../middlewares/error.middleware');
+const swaggerDocument = require('../swagger.json')
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 app.use('/login', authRouter);
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(errorHandler);
