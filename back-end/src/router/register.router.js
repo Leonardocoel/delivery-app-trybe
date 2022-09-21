0e278bd198841ee9a32cb5317938f889b26f10d3
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2022-09-21 17:43:50 -0300

    feat: :rocket: Created validations for register route

--- back-end/src/router/register.router.js
+++ back-end/src/router/register.router.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
-const authController = require('../controllers/register.controller');
+const registerController = require('../controllers/register.controller');
 
 const router = Router();
 
-router.post('/', authController.register);
+router.post('/', registerController.register);
 
 module.exports = router;
--- back-end/src/services/register.service.js
+++ back-end/src/services/register.service.js
@@ -5,9 +5,9 @@ const registerNewUser = async (username, email, password, role) => {
   await users.create({ username, email, password, role });
 
   const newUser = await users.findOne({ where: { email, username } });
-  if (!newUser) {
-    const e = new Error('Invalid fields');
-    e.name = 'ValidationError';
+  if (newUser) {
+    const e = new Error('User already exists');
+    e.name = 'Conflict';
     throw e;
   }
 };
