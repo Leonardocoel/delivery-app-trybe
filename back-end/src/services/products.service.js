7378c8f8fc9b72c402eeecf8dfdc0a3a3f7b4642
MatheusCabralAraujo <matheuscabral20114@gmail.com>
2022-09-22 15:47:34 -0300

    feat: :rocket: Added post,put and delete routes for products

--- back-end/src/services/products.service.js
+++ back-end/src/services/products.service.js
@@ -5,7 +5,7 @@ const getAll = async () => {
   return result;
 };
 
-const getById = async (id) => {
+const getProductById = async (id) => {
   const user = await products.findOne(
     { where: { id },
   },
@@ -13,7 +13,36 @@ const getById = async (id) => {
   return user;
 };
 
+const createProduct = async (data) => {
+  const newProduct = await products.create(data);
+  return newProduct;
+};
+
+const updateProduct = async (id, data) => {
+  const result = await getProductById(id);
+  if (!result) {
+    const e = new Error('Product does not exist');
+    e.name = 'NotFoundError';
+    throw e;
+  }
+  const resultUpdated = result.update({ data });
+  return resultUpdated;
+};
+
+const deleteProductById = async (id) => {
+  const product = await getProductById(id);
+  if (!product) {
+    const e = new Error('Product does not exist');
+    e.name = 'NotFoundError';
+    throw e;
+  }
+  return product.destroy({ where: { id: [id] } });  
+};
+
 module.exports = {
   getAll,
-  getById,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProductById,
 };
