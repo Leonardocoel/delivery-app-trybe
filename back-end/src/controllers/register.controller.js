3c474861149c74feaf1daf21d75a8a5981acb8a5
Leonardo C <leonardocoel98@gmail.com>
2022-09-25 14:16:48 -0300

    refactor: changes names, types (sequelize) and small code changes to improve readability

--- back-end/src/controllers/register.controller.js
+++ back-end/src/controllers/register.controller.js
@@ -2,7 +2,7 @@ const registerService = require('../services/register.service');
 
 const register = async (req, res, next) => {
  const { name, email, password } = req.body;
- console.log(name);
+
   try { 
     const user = await registerService.registerNewUser(name, email, password);
     return res.status(201).json(user);
--- back-end/src/router/register.router.js
+++ back-end/src/router/register.router.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const registerController = require('../controllers/register.controller');
-const validateRegisterFormat = require('../middlewares/register.validations');
+const { validateUserRegistration } = require('../middlewares/register.validations');
 
 const router = Router();
 
-router.post('/', validateRegisterFormat.validateUserRegistration, registerController.register);
+router.post('/', validateUserRegistration, registerController.register);
 
 module.exports = router;
