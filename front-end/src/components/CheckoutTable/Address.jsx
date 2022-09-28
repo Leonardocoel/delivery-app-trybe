a285187c7f50105df8e14a9012a28623be5fe34f
Oswaldo Zegarra <edwingzegarra@gmail.com>
2022-09-28 20:41:06 -0300

    fix: linter

--- front-end/src/components/CheckoutTable/Address.jsx
+++ front-end/src/components/CheckoutTable/Address.jsx
@@ -2,7 +2,6 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CustomerContext from '../../context/CostumerContext';
 import { requestGet, requestPost, setToken } from '../../services/requests';
-import Header from '../Header';
 
 export default function Address() {
   const navigate = useNavigate();
