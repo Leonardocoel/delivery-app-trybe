b452538136ec50b628147c32a4ab08568b471e2b
Oswaldo Zegarra <edwingzegarra@gmail.com>
2022-09-28 20:32:37 -0300

    feat: backend integration

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -2,20 +2,28 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Header from '../components/Header';
 import { requestGet } from '../services/requests';
+import convertValue from '../utils/convertValue';
 
 const ID_BASE = 'seller_order_details__';
 
 export default function OrderDetails() {
   const [order, setOrder] = useState();
+  const [status, setStatus] = useState('Pendente');
   const { id: idParams } = useParams();
 
   useEffect(() => {
     const requestOrder = async () => {
-      const data = await requestGet(`/sales/${idParams}`);
+      const data = await requestGet(`/customer/orders/${idParams}`);
+      console.log(data);
       setOrder(data);
+      setStatus(data.status);
     };
     requestOrder();
   }, [idParams]);
+
+  useEffect(() => {
+    // requestPatch(endpoint, status);
+  }, [status]);
 
   return (
     <main>
@@ -28,23 +36,26 @@ export default function OrderDetails() {
               {`PEDIDO ${order.id}`}
             </p>
             <p data-testid={ `${ID_BASE}element-order-details-label-order-date` }>
-              {new Date(order.saleDate).toLocaleDateString()}
-              {/* fonte: https://pt.stackoverflow.com/questions/6526/como-formatar-data-no-javascript */}
+              {new Date(order.sale_date).toLocaleDateString()}
             </p>
             <p
               data-testid={ `${ID_BASE}element-order-details-label-delivery-status` }
             >
-              {order.status}
+              {status}
             </p>
             <button
               data-testid={ `${ID_BASE}button-preparing-check` }
               type="button"
+              disabled={ status !== 'Pendente' }
+              onClick={ () => setStatus('Preparando') }
             >
               Preparar pedido
             </button>
             <button
               data-testid={ `${ID_BASE}button-dispatch-check` }
               type="button"
+              disabled={ status !== 'Preparando' }
+              onClick={ () => setStatus('Em Trânsito') }
             >
               Saiu para entrega
             </button>
@@ -60,7 +71,7 @@ export default function OrderDetails() {
               </tr>
             </thead>
             <tbody>
-              {order.produtos.map((elem, index) => (
+              {order.products.map((elem, index) => (
                 <tr key={ elem.id }>
                   <td
                     data-testid={ `${ID_BASE}element-order-table-item-number-${index}` }
@@ -75,28 +86,26 @@ export default function OrderDetails() {
                   <td
                     data-testid={ `${ID_BASE}element-order-table-quantity-${index}` }
                   >
-                    {elem.quantity}
+                    {elem.SalesProduct.quantity}
                   </td>
                   <td
                     data-testid={ `${ID_BASE}element-order-table-unit-price-${index}` }
                   >
-                    {`R$ ${elem.price.toFixed(2).replace('.', ',')}`}
+                    {convertValue(elem.price)}
                   </td>
                   <td
                     data-testid={ `${ID_BASE}element-order-table-sub-total-${index}` }
                   >
-                    {`R$ ${(elem.price * elem.quantity)
-                      .toFixed(2)
-                      .replace('.', ',')}`}
+                    {convertValue(elem.price * elem.SalesProduct.quantity)}
                   </td>
                 </tr>
               ))}
             </tbody>
           </table>
           <p
-            data-testid={ `${ID_BASE}element-order-total-prices` }
+            data-testid={ `${ID_BASE}element-order-total-price` }
           >
-            {`Total: R$ ${order.totalPrice.toFixed(2).replace('.', ',')}`}
+            {`Total: ${convertValue(order.totalPrice)}`}
 
           </p>
         </>
